540284c35e632ccddc1b2d6fe8d14cd49570a3ed
alise-fox <alisefox14@mail.ru>
2025-05-22 18:37:07 +0500

    grpc

---
+++ exporter/grpc_client.py
@@ -0,0 +1,31 @@
+import grpc
+import books_pb2
+import books_pb2_grpc
+from utils import read_books, read_config, create_table, fill_example_data
+
+def run():
+    config = read_config("config/config.yaml")
+    channel = grpc.insecure_channel(f"{config['grpc']['host']}:{config['grpc']['port']}")
+    stub = books_pb2_grpc.BookTransferStub(channel)
+
+    create_table(config)
+    fill_example_data(config)
+
+    for row in read_books(config['sqlite_db']):
+        # row: (id, title, author, genre, publisher, year, borrower_name, borrower_address, borrow_date)
+        book = books_pb2.BookData(
+            id=row[0],
+            title=row[1],
+            author=row[2],
+            genre=row[3],
+            publisher=row[4],
+            year=row[5],
+            borrower_name=row[6],
+            borrower_address=row[7],
+            borrow_date=row[8]
+        )
+        reply = stub.SendBook(book)
+        print("Ответ от сервера:", reply.success, reply.message)
+
+if __name__ == "__main__":
+    run()
---
+++ importer/grpc_server.py
@@ -0,0 +1,33 @@
+import grpc
+from concurrent import futures
+import books_pb2
+import books_pb2_grpc
+from db_postgres import insert_normalized
+from utils import read_config
+
+class BookTransferServicer(books_pb2_grpc.BookTransferServicer):
+    def __init__(self, config):
+        self.config = config
+
+    def SendBook(self, request, context):
+        row = (
+            request.id, request.title, request.author, request.genre, request.publisher,
+            request.year, request.borrower_name, request.borrower_address, request.borrow_date
+        )
+        try:
+            insert_normalized(self.config, row)
+            return books_pb2.BookReply(success=True, message="Inserted successfully")
+        except Exception as e:
+            return books_pb2.BookReply(success=False, message=str(e))
+
+def serve():
+    config = read_config("../config/config.yaml")
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    books_pb2_grpc.add_BookTransferServicer_to_server(BookTransferServicer(config), server)
+    server.add_insecure_port('[::]:50051')  # слушаем все интерфейсы на порту 50051
+    print("gRPC сервер запущен на порту 50051")
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == "__main__":
+    serve()
