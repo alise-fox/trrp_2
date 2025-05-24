b3527796411e081b82e4c5f4d55efa8b7a561374
alise-fox <alisefox14@mail.ru>
2025-05-24 08:46:58 +0500

    fix stream

--- exporter/grpc_client.py
+++ exporter/grpc_client.py
@@ -2,17 +2,10 @@ import grpc
 from grpc_alice import books_pb2, books_pb2_grpc
 from exporter.utils import read_books, read_config, create_table, fill_example_data
 
-def run():
-    config = read_config("config/config.yaml")
-    channel = grpc.insecure_channel(f"{config['grpc']['host']}:{config['grpc']['port']}")
-    stub = books_pb2_grpc.BookTransferStub(channel)
 
-    create_table(config)
-    fill_example_data(config)
-
-    for row in read_books(config['sqlite_db']):
-        # row: (id, title, author, genre, publisher, year, borrower_name, borrower_address, borrow_date)
-        book = books_pb2.BookData(
+def book_generator(sqlite_db):
+    for row in read_books(sqlite_db):
+        yield books_pb2.BookData(
             id=row[0],
             title=row[1],
             author=row[2],
@@ -23,7 +16,29 @@ def run():
             borrower_address=row[7],
             borrow_date=row[8]
         )
-        reply = stub.SendBook(book)
+
+
+def run():
+    config = read_config("config/config.yaml")
+    channel = grpc.insecure_channel(f"{config['grpc']['host']}:{config['grpc']['port']}")
+    stub = books_pb2_grpc.BookTransferStub(channel)
+
+    create_table(config)
+    fill_example_data(config)
+
+    for row in read_books(config['sqlite_db']):
+        # book = books_pb2.BookData(
+        #     id=row[0],
+        #     title=row[1],
+        #     author=row[2],
+        #     genre=row[3],
+        #     publisher=row[4],
+        #     year=row[5],
+        #     borrower_name=row[6],
+        #     borrower_address=row[7],
+        #     borrow_date=row[8]
+        # )
+        reply = stub.SendBook(book_generator())
         print("Ответ от сервера:", reply.success, reply.message)
 
 if __name__ == "__main__":
--- importer/grpc_server.py
+++ importer/grpc_server.py
@@ -8,23 +8,24 @@ class BookTransferServicer(books_pb2_grpc.BookTransferServicer):
     def __init__(self, config):
         self.config = config
 
-    def SendBook(self, request, context):
-        row = (
-            request.id, request.title, request.author, request.genre, request.publisher,
-            request.year, request.borrower_name, request.borrower_address, request.borrow_date
-        )
-        try:
-            insert_normalized(self.config, row)
-            return books_pb2.BookReply(success=True, message="Inserted successfully")
-        except Exception as e:
-            return books_pb2.BookReply(success=False, message=str(e))
+    def SendBook(self, request_iterator, context):
+        for book in request_iterator:
+            row = (
+                book.id, book.title, book.author, book.genre, book.publisher,
+                book.year, book.borrower_name, book.borrower_address, book.borrow_date
+            )
+            try:
+                insert_normalized(self.config, row)
+                return books_pb2.BookReply(success=True, message="Inserted successfully")
+            except Exception as e:
+                return books_pb2.BookReply(success=False, message=str(e))
 
 def serve():
     config = read_config("config/config.yaml")
     create_table(config)
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     books_pb2_grpc.add_BookTransferServicer_to_server(BookTransferServicer(config), server)
-    server.add_insecure_port('[::]:50051')  # слушаем все интерфейсы на порту 50051
+    server.add_insecure_port('[::]:50051') 
     print("gRPC сервер запущен на порту 50051")
     server.start()
     server.wait_for_termination()
